3f3acc1a6a2d78fc0e03925643fe98d095823930
Albertliucheng <40171032+Albertliucheng@users.noreply.github.com>
2023-03-08 00:23:45 +0800

    Add files via upload

---
+++ w1/Counter.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+contract Counter {
+    uint public a;
+
+ function add(uint _a) public {
+    a= _a;
+ }
+}
+
+
+//tx_hash: 0x47f0019293524be72883f79a3eeb64c5a8009199170d7c8a0933e94d255331d6
